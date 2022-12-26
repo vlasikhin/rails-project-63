c55e85511154f4203c5378260fed22b0a9894b5c
Pavel Vlasikhin <pavel.vlasikhin@gmail.com>
2022-12-26 18:22:45 +0300

    initial comit

---
+++ test/test_hexlet_code.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestHexletCode < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::HexletCode::VERSION
+  end
+end
