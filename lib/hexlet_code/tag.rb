95b45aea1ac0168c289b3bc02adab347759c5020
Pavel Vlasikhin <pavel.vlasikhin@gmail.com>
2022-12-26 19:34:23 +0300

    add: tag bulder

---
+++ lib/hexlet_code/tag.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module HexletCode
+  class Tag
+    def self.build(name, attributes = {}, &)
+      new(name, attributes, &).to_s
+    end
+
+    def initialize(name, attributes = {}, &body)
+      @name = name
+      @attributes = attributes
+      @body = body
+    end
+
+    def to_s
+      content = @body ? @body.call : ""
+
+      if empty_tags.include?(@name)
+        "<#{@name}#{attributes} />"
+      else
+        "<#{@name}#{attributes}>#{content}</#{@name}>"
+      end
+    end
+
+    private
+
+    def attributes
+      return "" if @attributes.empty?
+
+      @attributes.map do |key, value|
+        " #{key}=\"#{value}\""
+      end.join
+    end
+
+    def empty_tags
+      %w[br hr img meta input]
+    end
+  end
+end
